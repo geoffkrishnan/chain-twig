26ffce97e2364abc629df0cb30bba8b8ccd7fa15
Geoff Krishnan <geoffrykrishnan@gmail.com>
2025-11-07 06:24:38 -0600

    feat: Add copy link buttons

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+document.querySelectorAll('.copy-link').forEach(button => {
+  const wrapper = button.closest('.link-wrapper');
+  const link = wrapper.querySelector('a').href;
+  const img = button.querySelector('img');
+  const tooltip = button.querySelector('.tool-tip');
+
+  button.addEventListener('click', e => {
+    e.preventDefault();
+    navigator.clipboard.writeText(link).then(() => {
+      img.src = "images/checkmark.png";
+      button.classList.add('show-tool-tip');
+      setTimeout(() => {
+        img.src = "images/link.png";
+        button.classList.remove('show-tool-tip');
+      }, 1500);
+    });
+  });
+});
+
